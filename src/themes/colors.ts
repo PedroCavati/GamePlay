bc92db60244ed04cf9ff81b1d0ea579ce19259b7
Pedro Sousa <pedroc.sousa@outlook.com>
2026-09-25 18:28:03 -0300

    Added theme support to the app and started migrating it

---
+++ src/themes/colors.ts
@@ -0,0 +1,8 @@
+export const Colors = {
+    beyondNavy: "#0E1647",
+    coolDarkBlue: "#0A1033",
+
+    white: "#FFFFFF",
+
+    backgroundGradient: ["#0E1647", "#0A1033"],
+}
---
+++ src/themes/fonts.ts
@@ -0,0 +1,15 @@
+import { TextStyle } from "react-native";
+
+export const Fonts = {
+  title: {
+    fontFamily: 'Rajdhani',
+    fontSize: 40,
+    fontWeight: '700',
+  } satisfies TextStyle,
+
+  subtitle: {
+    fontFamily: 'Inter',
+    fontSize: 15,
+    fontWeight: '400',
+  } satisfies TextStyle,
+}
